4358cde146943d5a4e11a3198e0f8a1df47097ea
Michal Josef Špaček <michal.spacek@gooddata.com>
2017-11-29 22:15:26 +0100

    Add documentation
    Signed-off-by: Michal Josef Špaček <michal.spacek@gooddata.com>

--- GraphViz.pm
+++ GraphViz.pm
@@ -112,3 +112,135 @@ sub _add_edge {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+SCPN::Schema::GraphViz - Class for drawing SCPN::Schema via GraphViz.
+
+=head1 SYNOPSIS
+
+ use SCPN::GraphViz;
+ my $obj = SCPN::GraphViz->new(%params);
+ $obj->to_png($scpn_schema, $output_png_file);
+
+=head1 METHODS
+
+=over 8
+
+=item C<new(%params)>
+
+ Constructor.
+
+=over 8
+
+=item * C<graph_title>
+
+ Graph title.
+ Default value is 'Petri net'.
+
+=item * C<rank_dir>
+
+ Rank direction.
+ Default value is 'LR'.
+
+=item * C<print_titles>
+
+ Print titles.
+ Default value is 0.
+
+=back
+
+=item C<to_png($scpn_schema)>
+
+ Generate diagram to PNG file via GraphViz.
+ Returns GraphViz object.
+
+=back
+
+=head1 ERRORS
+
+ new():
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+
+ to_png():
+         to_png: Bad SCPN::Schema object.
+
+=head1 EXAMPLE
+
+ use strict;
+ use warnings;
+
+ use SCPN::Schema;
+ use SCPN::Schema::GraphViz;
+ use File::Temp;
+ use IO::Barf qw(barf);
+
+ # Schema.
+ my $schema = <<'END';
+ {
+         "events":{
+                 "copier":{
+                         "input_edges":[
+                                 {"condition":"entry_point_condition"}
+                         ],
+                         "output_edges":[
+                                 {"condition":"copy_resutls", "count":3}
+                         ]
+                 }
+         },
+         "case":{
+                 "entry_point_condition":{
+                         "init_config":{"value":"bullet"}
+                 }
+         }
+ }
+ END
+ my $temp1 = File::Temp->new->filename;
+ barf($temp1, $schema);
+
+ # Schema object.
+ my $scpn_schema = SCPN::Schema->new;
+ $scpn_schema->build_schema_from_json($temp1);
+ unlink $temp1;
+
+ # Object.
+ my $obj = SCPN::Schema::GraphViz->new;
+
+ # Generate PNG file.
+ my $temp2 = File::Temp->new->filename;
+ $obj->to_png($scpn_schema, $temp2);
+
+ # List file.
+ system('ls -l '.$temp2);
+
+ # Output like:
+ # -rw-r--r-- 1 skim skim 508 Nov 29 22:13 /tmp/PsYcWWsZmI
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<GraphViz2>,
+L<Mojo::Exception>,
+L<Scalar::Util>.
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © Michal Špaček 2017
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
